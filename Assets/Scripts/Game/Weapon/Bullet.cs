e31769a96d4520102199e6f4014a9e01bfc3625f
1138 <dakuansar7@gmail.com>
2024-11-29 17:21:09 +0500

    refactor Bullet prefab fix bullet collision and add penetration parameter for bullets

--- Assets/Scripts/Game/Weapon/Bullet.cs
+++ Assets/Scripts/Game/Weapon/Bullet.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private CharacterController controller;
+    [SerializeField] private Rigidbody rb;
 
     public event Action<Bullet> OnHit;
 
     private const float Speed = 100;
     private Character player;
     private float range;
+    private float penetratedEnemies;
+    private float penPower;
 
     public void Start()
     {
@@ -17,24 +19,32 @@ public class Bullet : MonoBehaviour
         Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
 
         range = MetaManager.Instance.WeaponData.AttackRange;
+        penPower = MetaManager.Instance.WeaponData.PenetrationPower;
     }
 
     public void Update()
     {
-        controller.Move(transform.forward * (Speed * Time.deltaTime));
-
+        rb.velocity = transform.forward * Speed;
         if (Vector3.Distance(transform.position, player.transform.position) > range)
         {
             OnHit?.Invoke(this);
         }
     }
 
-    public void OnControllerColliderHit(ControllerColliderHit hit)
+    public void OnTriggerEnter(Collider other)
     {
-        if (hit.collider.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy"))
         {
-            player.DamageComponent.DealDamage(hit.collider.gameObject.GetComponent<Enemy>());
-            OnHit?.Invoke(this);
+            player.DamageComponent.DealDamage(other.gameObject.GetComponent<Enemy>());
+            if (penPower > 0 && penetratedEnemies < penPower)
+            {
+                penetratedEnemies++;
+            }
+            else
+            {
+                OnHit?.Invoke(this);
+                penetratedEnemies = 0;
+            }
         }
     }
 }
--- Assets/Scripts/Game/Weapon/Data/ShotgunData.cs
+++ Assets/Scripts/Game/Weapon/Data/ShotgunData.cs
@@ -2,14 +2,14 @@
 
 public class ShotgunData : WeaponData
 {
-    // 1 Base DPS: 34
-    // Full Base DPS: 170 (DPS * ProjectileCount)
+    // 1 Base DPS: 30
+    // Full Base DPS: 150 (DPS * ProjectileCount)
     public ShotgunData()
     {
         WeaponId = 1;
         WeaponSprite = Resources.Load<Sprite>("Sprites/Weapons/SPAS-12_Shotgun");
-        Damage = 17;
-        AttackSpeed = 0.5f;
+        Damage = 45;
+        AttackSpeed = 1.5f;
         ReloadTime = 2.5f;
         AttackRange = 50;
         ProjectilesCount = 5;
